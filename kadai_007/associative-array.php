f4dc6af9a34ca4f53f0eeb02e9abcf809856adf9
Di-cek68 <dytydyty6791@gmail.com>
2025-02-05 22:06:48 +0900

    Kadai_007/associative-array.php
    Kadai_007/associative-array.php

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP KADAI_007</title>
+</head>
+<body>
+
+<?php
+$item = array(
+  "name" => "onion",
+  "price" => 200,
+  "weight" => 160
+);
+
+echo "<pre>";
+print_r($item);
+echo "</pre>";
+?>
+
+</body>
+</html>
